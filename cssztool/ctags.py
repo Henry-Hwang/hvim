3fc2abec56760aa54631c8fb915ded4eb20ce1a0
Henry Huang <henry.huang@cirrus.com>
2019-12-13 23:49:49 +0800

    csszt: updated

---
+++ cssztool/ctags.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import os
+import sys
+import platform
+import time
+import datetime
+import shutil
+import argparse
+import hashlib
+from decimal import Decimal
+import subprocess
+from amps import Amps
+from playback import Playback
+from config import Config
+from andevice import Andevice
+from tool import Tool
+
+CTAG_DICT = {
+       "kernel" : ["sound",
+                   "include",
+                   "drivers/misc",
+                   "drivers/base",
+                   "drivers/mfd",
+                   ],
+       "all" : ["."],
+}
+class Ctags:
+	def __init__(self):
+		self.conf = CTAG_DICT
+		pass
+	def create_ctags(self, args):
+		model = "ctags -R @DIRS ; ls -l tags"
+		paths = " ".join(self.conf[args])
+		model = model.replace("@DIRS", paths)
+		print(model)
+		os.system(model)
---
+++ cssztool/release.bat
@@ -0,0 +1,3 @@
+pyinstaller --onefile csszt.py
+copy "dist\csszt.exe" "C:\Program Files\Csszt\"
+copy "csszt_conf.json" "C:\Program Files\Csszt\"
